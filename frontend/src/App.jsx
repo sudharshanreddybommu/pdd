c5b006b39efda9dd46cc5654a3edee0c55fe330c
sudharshanreddybommu <sudharshanreddybommu1@gmail.com>
2026-08-03 19:11:33 +0530

    Align CURRENT_VERSION constant in App.jsx to 1.0.1 to match server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import { getAppVersion } from './services/api'
 
-const CURRENT_VERSION = '1.0.0'
+const CURRENT_VERSION = '1.0.1'
 
 const PatientPrivateRoute = ({ children }) => {
   const token = localStorage.getItem('patientToken')
